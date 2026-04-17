1369f79c427dd734264e684279fddfce6032aab4
nvnx7 <thenvnsahu@gmail.com>
2026-04-18 01:11:23 +0530

    feat: add user order api

--- modules/orderbook/src/query.rs
+++ modules/orderbook/src/query.rs
@@ -1,31 +1,38 @@
-use crate::{MarketSideKey, OrderbookModule, PriceLevelKey};
+use crate::{MarketSideKey, Order, OrderbookModule, PriceLevelKey};
 use market::MarketId;
-use shared_types::{Price, Side};
-use sov_modules_api::prelude::axum::{
-    extract::ws::{Message, WebSocket, WebSocketUpgrade},
-    extract::Query,
-    response::{IntoResponse, Response},
-    routing::{any, get},
-    Json, Router,
-};
+use shared_types::{OrderId, Price, Side};
 use sov_modules_api::prelude::tokio::time::{sleep, Duration};
 use sov_modules_api::prelude::utoipa::openapi::OpenApi;
+use sov_modules_api::prelude::{
+    axum::{
+        extract::{
+            ws::{Message, WebSocket, WebSocketUpgrade},
+            Query,
+        },
+        response::{IntoResponse, Response},
+        routing::{any, get},
+        Json, Router,
+    },
+    UnwrapInfallible,
+};
 use sov_modules_api::rest::utils::{errors, ApiResult, Path};
 use sov_modules_api::rest::{ApiState, HasCustomRestApi};
 use sov_modules_api::{ApiStateAccessor, Spec};
 use std::collections::HashMap;
 
-impl<S: Spec> OrderbookModule<S> {
-    async fn route_status() -> ApiResult<String> {
-        Ok(Json("OK".to_string()))
-    }
+#[derive(Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize, Clone)]
+struct UserOrdersQueryParams<S: Spec> {
+    user_address: S::Address,
+    market_id: Option<MarketId>,
+}
 
+impl<S: Spec> OrderbookModule<S> {
     // WebSocket handler for canonical book data
     async fn route_ws(
+        Query(params): Query<HashMap<String, String>>,
         ws: WebSocketUpgrade,
         state: ApiState<S, Self>,
         acc: ApiStateAccessor<S>,
-        Query(params): Query<HashMap<String, String>>,
     ) -> impl IntoResponse {
         let market_id = params.get("market_id").and_then(|s| s.parse::<u64>().ok());
         ws.on_upgrade(move |socket| Self::handle_orderbook_socket(socket, state, acc, market_id))
@@ -89,6 +96,45 @@ impl<S: Spec> OrderbookModule<S> {
         }
         levels
     }
+
+    async fn route_user_orders(
+        params: Query<UserOrdersQueryParams<S>>,
+        state: ApiState<S, Self>,
+        mut acc: ApiStateAccessor<S>,
+    ) -> ApiResult<Vec<Order<S>>> {
+        let user_address = params.user_address;
+        let maybe_market_id = params.market_id;
+
+        let orders_ids = state
+            .user_orders
+            .get(&user_address, &mut acc)
+            .unwrap()
+            .unwrap_or_default();
+
+        let mut orders = Vec::new();
+        for oid in orders_ids {
+            let order = state
+                .orders
+                .get(&oid, &mut acc)
+                .unwrap_infallible()
+                .ok_or_else(|| errors::not_found_404("Order", oid))?;
+
+            let include = match maybe_market_id {
+                Some(market_id) => order.market_id == market_id,
+                None => true,
+            };
+
+            if include {
+                orders.push(order);
+            }
+        }
+
+        Ok(orders.into())
+    }
+
+    async fn route_status() -> ApiResult<String> {
+        Ok(Json("OK".to_string()))
+    }
 }
 
 impl<S: Spec> HasCustomRestApi for OrderbookModule<S> {
@@ -98,6 +144,7 @@ impl<S: Spec> HasCustomRestApi for OrderbookModule<S> {
         Router::new()
             .route("/status", get(Self::route_status))
             .route("/ws", any(Self::route_ws))
+            .route("/user-orders", get(Self::route_user_orders))
             .with_state(state.with(self.clone()))
     }
 
