3b61c8dec81d883d71344d690c43b854fce71052
nvnx7 <thenvnsahu@gmail.com>
2026-05-15 00:25:26 +0530

    chore: cleanup

--- crates/circuits/script/src/main.rs
+++
@@ -1,9 +0,0 @@
-//! Host script for proving/verifying UTXO transactions with SP1.
-//!
-//! Usage:
-//!   RUST_LOG=info cargo run --release -- --execute
-//!   RUST_LOG=info cargo run --release -- --prove
-
-fn main() {
-    todo!("Implement host-side prover script using sp1-sdk + circuits-lib");
-}
